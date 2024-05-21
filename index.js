62f9b1c9975d105154142826a5db81525e445eb1
Anas748 <100359811+Anas748@users.noreply.github.com>
2024-05-21 14:24:36 +0100

    created a server
    server implementation listen to request and send index.html page in response

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
 const app = express();
 const PORT = 3000;
 const path = require('path');
+const WebSocket = require('ws');
+
+
+const server = http.createServer(app)
+
+const wsServer = new WebSocket.Server( { noServer: true } );
+
+app.use(express.static(path.resolve("")))
+app.get("/",(req,res)=>{
+    return res.sendFile("index.html")
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
