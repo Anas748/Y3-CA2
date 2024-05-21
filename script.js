8d794f602507c552e93c17c1b2115ff528db8fc2
Anas748 <100359811+Anas748@users.noreply.github.com>
2024-05-21 18:48:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById('joinForm').addEventListener('submit', function(event) {
         .then(response => response.json())
         .then(data => {
             if(data.success) {
-                window.location.href = `/game.html`;
+                window.location.href = `/game.html?gameId=${data.gameId}`;
             } else {
                 alert('Error joining the game.');
             }
