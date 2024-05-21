a8030d3e044bd9e89f34e92c2f00c8c19c1818e1
Anas748 <100359811+Anas748@users.noreply.github.com>
2024-05-21 14:43:18 +0100

    added a post method in javascript
    on client side i have made a function which is sending data to server

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('joinForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const playerName = document.getElementById('playerName').value;
+
+    fetch('/join', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: playerName })
+        
+        
+    });
+});
