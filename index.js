ab7e131889b05d5be2f6d427e47b12ccfe20ee99
Anas748 <100359811+Anas748@users.noreply.github.com>
2024-05-21 16:11:16 +0100

    player in game
    so max player can play a game is 4 and adding submitted name of player to players arr

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const WebSocket = require('ws');
 
 const server = http.createServer(app);
 
-const wsServer = new WebSocket.Server( { noServer: true } );
+const wsServer = new WebSocket.Server();
+const MAX_PLAYERS = 4; //MAx Player in a game is 4 
+let players = []; // Array to keep track of players
 
 app.use(express.static(path.resolve("")));
 app.get("/",(req,res)=>{
@@ -22,6 +24,8 @@ app.post('/join', (req, res) => {
     if (!playerName || playerName=="") {
         return res.json({ success: false, message: 'Name is required' });
     }
+    // Add the player to the array
+    players.push(playerName);
     res.json({ success: true, player: playerName });
     
 });
