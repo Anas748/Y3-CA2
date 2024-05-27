8c8b11ae29ac31b7cd6d0b5f7ab2c586b0155ab4
Anas748 <100359811+Anas748@users.noreply.github.com>
2024-05-28 00:10:39 +0100

    added handle method for conn and disconnections

--- server.js
+++ server.js
@@ -13,3 +13,30 @@ const playerStats = {};
 const players = {};
 const maxPlayers = 4;
 let currentPlayerTurn = null;
+
+io.on('connection', (socket) => {
+    socket.on('joinGame', (playerName) => {
+        if (Object.keys(players).length >= maxPlayers) {
+            socket.emit('full');
+            socket.disconnect();
+            return;
+        }
+
+    });
+
+
+        socket.on('disconnect', () => {
+            if (players[socket.id]) {
+                delete players[socket.id];
+            }
+    
+            if (Object.keys(players).length === 0) {
+                gameBoard = Array.from({ length: 10 }, () => Array(10).fill(null));
+                totalGames++;
+            } else if (currentPlayerTurn === socket.id) {
+                updateTurnOrder();
+            }
+    
+            io.emit('updateBoard', gameBoard);
+    });
+});        
