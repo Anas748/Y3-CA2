5f6e9b50c202d25b45a61529bdf26dc7c5da0789
Anas748 <100359811+Anas748@users.noreply.github.com>
2024-05-28 00:07:50 +0100

    game board array
    adding a gameboard array to fill 2d array and some required variables

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static('client'));
+
+let totalGames = 0;
+let gameBoard = Array.from({ length: 10 }, () => Array(10).fill(null));
+const playerStats = {};
+const players = {};
+const maxPlayers = 4;
+let currentPlayerTurn = null;
