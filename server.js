892744e3a5c98e3b2129daeec6df14d9ac58f8a2
Anas748 <100359811+Anas748@users.noreply.github.com>
2024-05-30 18:04:58 +0100

    player will be added to game

--- server.js
+++ server.js
@@ -21,6 +21,37 @@ class Game {
         this.maxActionsPerTurn = 10;
         this.gameEnded = false;
     }
+    // Adding a player to the game
+    addPlayer(socket, playerName) {
+        if (Object.keys(this.players).length >= this.maxPlayers) {
+            socket.emit('full');
+            socket.disconnect();
+            return;
+        }
+
+        this.players[socket.id] = { id: socket.id, name: playerName, monsters: [], lostMonsters: 0, eliminated: false };
+        this.playerStats[socket.id] = { wins: 0, losses: 0 };
+
+        const playerIndex = Object.keys(this.players).indexOf(socket.id);
+        const playerCount = Object.keys(this.players).length;
+
+        if (Object.keys(this.players).length === 1) {
+            this.currentPlayerTurn = socket.id;
+            this.actionCounter = 0;
+        }
+
+        socket.join(this.gameId);
+        socket.emit('updateBoard', this.gameBoard);
+        socket.emit('updateStats', {
+            totalGames: this.totalGames,
+            playerWins: this.playerStats[socket.id].wins,
+            playerLosses: this.playerStats[socket.id].losses,
+        });
+
+        io.to(this.gameId).emit('highlightEdge', { playerIndex, playerCount });
+        io.to(this.gameId).emit('turnUpdate', this.players[this.currentPlayerTurn].name);
+    }
+
 }
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
