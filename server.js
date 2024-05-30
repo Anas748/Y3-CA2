b7958100c690725d5bc00ab9f664befbf332c380
Anas748 <100359811+Anas748@users.noreply.github.com>
2024-05-30 17:55:46 +0100

    game class
    this game class will make more games as required

--- server.js
+++ server.js
@@ -7,7 +7,21 @@ const io = socketIo(server);
 const PORT = 3000;
 
 app.use(express.static('client'));
-
+// Defining the Game class
+class Game {
+    constructor(gameId) {
+        this.gameId = gameId;
+        this.totalGames = 0;
+        this.gameBoard = Array.from({ length: 10 }, () => Array(10).fill(null));
+        this.playerStats = {};
+        this.players = {};
+        this.maxPlayers = 4;
+        this.currentPlayerTurn = null;
+        this.actionCounter = 0;
+        this.maxActionsPerTurn = 10;
+        this.gameEnded = false;
+    }
+}
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
